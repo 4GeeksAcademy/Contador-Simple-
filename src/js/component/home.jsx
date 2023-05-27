2207ba53672efa805dd0afd879f47461d0f77b41
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-27 11:26:22 +0000

    simplecounter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import {FaClock } from 'react-icons/fa';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const SecondsCounter = () => {
+  const [time, setTime] = useState({
+    weeks: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prevTime) => {
+        const newTime = { ...prevTime };
+        newTime.seconds += 1;
+
+        if (newTime.seconds === 60) {
+          newTime.seconds = 0;
+          newTime.minutes += 1;
+        }
+        if (newTime.minutes === 60) {
+          newTime.minutes = 0;
+          newTime.hours += 1;
+        }
+        if (newTime.hours === 24) {
+          newTime.hours = 0;
+          newTime.days += 1;
+        }
+        if (newTime.days === 7) {
+          newTime.days = 0;
+          newTime.weeks += 1;
+        }
+
+        return newTime;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="calendar">
+      <div className="time">
+	  <div className="clock-icon"><FaClock /></div>
+        <div>{time.weeks} </div>
+        <div>{time.days} </div>
+        <div>{time.hours} </div>
+        <div>{time.minutes} </div>
+        <div>{time.seconds} </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default SecondsCounter;
